4e721fcdce8a7c632cb78092f2f1540ec0eee073
Shuvo458 <155560696+Shuvo458@users.noreply.github.com>
2024-04-21 20:54:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// jq
+$(document).ready(function(){
+    
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    $('.count').counterUp({
+        delay: 10,
+        time: 1500
+    });
+})
